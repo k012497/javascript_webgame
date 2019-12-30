9ba09c42364944cefdc94bcdfacd9eda04d0c5dd
k012497 <k012497@gmail.com>
2019-12-30 16:43:42 +0900

    rock_scissor_paper (completed)

--- rock_scissor_paper.js
+++ rock_scissor_paper.js
@@ -1,44 +1,81 @@
 var pixel = '0';
 // 자바스크립트의 객체는 딕셔너리 자료구조 역할을 해줌 (1:1 매칭 표현)
 var handPixel = {
-    scissor : '0',
-    rock : '-190px',
-    paper : '-390px',
-    example : { // 1:n도 가능
-        ex1 : 1,
-        ex2 : 2
-    }
+    scissor : {
+      hand : 'scissor',
+      pixel : '0',
+      value : 1,
+    },
+    rock : {
+      hand : 'rock',
+      pixel : '-190px',
+      value : 2,
+    },
+    paper : {
+      hand : 'paper',
+      pixel : '-390px',
+      value : 3,
+    },
 };
 
-// // 딕셔너리의 단점 : 거꾸로 찾는 게 힘들다. - 다른 구조는 없나 ?
-// var hand = {
-//     '0' : '가위',
-//     '-190px' : '바위',
-//     '-390px' : '보',
-// }
+var handArray = Object.entries(handPixel);
 
-function computerPick(pixel){
-    return Object.entries(handPixel).find(function(v){
-        console.log('computerPick', v);
-        return v[1] === pixel;
-    });
+function handPicked(indicator, by){
+    return handArray.find(function(v){
+        // console.log('v', v);
+        // console.log('v[0]', v[0]);
+        // console.log('v[1]', v[1]);
+        switch (by) {
+          case 'user':
+            return v[0] === indicator;
+          case 'computer':
+            return v[1].pixel === indicator;
+          default:
+            return -1;
+        }
+    })[1];
+}
+
+function calculateResult(user, computer) {
+  var userValue = user.value;
+  var computerValue = computer.value;
+
+  switch (userValue - computerValue) {
+    case 0:
+      alert('draw!');
+      break;
+    case 1: case -2:
+      alert('win!');
+      break;
+    case -1: case 2:
+      alert('lose :(');
+      break;
+
+    default:
+      break;
+  }
 }
 
 setInterval(function (){
-    if(pixel === handPixel.scissor){
-        pixel = handPixel.rock;
-    }else if(pixel === handPixel.rock){
-        pixel = handPixel.paper;
+    if(pixel === handPixel.scissor.pixel){
+        pixel = handPixel.rock.pixel;
+    }else if(pixel === handPixel.rock.pixel){
+        pixel = handPixel.paper.pixel;
     }else{
-        pixel = handPixel.scissor;
+        pixel = handPixel.scissor.pixel;
     }
     document.querySelector('#computer').style.background = 'url(http://www.lg-sl.net/sl_image/ALMA/ALMA2018/ALMA201804/ALMA2018040001002.jpg) ' + pixel + ' 0';
-}, 1000);
+}, 70);
+
+// calculateResult
 
 document.querySelectorAll('.btn').forEach(function(btn){
     btn.addEventListener('click', function(){
-        var user = this.textContent;
-        var computer = computerPick(pixel)[0];
-        console.log(user, computer);
+        var userSelect = this.textContent;
+        var computer = handPicked(pixel, 'computer');
+        var user = handPicked(userSelect, 'user')
+
+        calculateResult(user, computer);
+        console.log(user.value, computer.value);
     });
 });
