6f2689711c4a6daaedbca4646b59dc56c5b568cf
k012497 <k012497@gmail.com>
2020-01-19 16:07:55 +0900

    move and merge number

--- 2048.js
+++ 2048.js
@@ -1,8 +1,11 @@
 var table = document.getElementById('table');
+var score = document.getElementById('score');
 var data = [];
 
 function initSetting(param){ 
-    var fragment = document.createDocumentFragment();
+    table.innerHTML = '';
+    score.textContent = '0';
+    var fragment = document.createDocumentFragment(); // 화면에 바로 추가하지 않고 프래그먼트(메모리) 이용
     [1, 2, 3, 4].forEach(function(){
         var colData = [];
         data.push(colData);
@@ -17,7 +20,7 @@ function initSetting(param){
     table.appendChild(fragment);
 }
 
-function makeRandom(){
+function makeRandomCell(){
     var blankArray = [];
     data.forEach(function(colData, i){
         colData.forEach(function (rowData, j) { 
@@ -26,10 +29,21 @@ function makeRandom(){
             }
          });
     });
-    var randomCell = blankArray[Math.floor(Math.random() * blankArray.length)];
-    data[randomCell[0]][randomCell[1]] = 2;
-    console.log("randomCell", randomCell);
-    drawing();
+
+    if(blankArray.length === 0){
+        alert("GAME OVER ! score:" + score.textContent);
+        data = [];
+        initSetting();
+        makeRandomCell();
+        drawing();
+    }else{
+        var randomCell = blankArray[Math.floor(Math.random() * blankArray.length)];
+        console.log(randomCell);
+        data[randomCell[0]][randomCell[1]] = 2;
+        console.log("randomCell", randomCell);
+        drawing();
+    }
+
 }
 
 function drawing(){
@@ -40,10 +54,141 @@ function drawing(){
             }else{
                 table.children[i].children[j].textContent = '';
             }
-        })
-    })
+        });
+    });
 }
 
+var startDrag = false;
+var startPosition;
+var endPosition;
+window.addEventListener('mousedown', function (e) { 
+    console.log('mousedown', e);
+    startDrag = true;
+    startPosition = [e.clientX, e.clientY];
+});
+
+window.addEventListener('mousemove', function (e) { 
+    // console.log('mousemove', e);
+});
+
+window.addEventListener('mouseup', function (e) { 
+    console.log('mouseup', e);
+    startDrag = false;
+    endPosition = [e.clientX, e.clientY];
+    var direction;
+    var xDiff = endPosition[0] - startPosition[0];
+    var yDiff = endPosition[1] - startPosition[1];
+    if(yDiff < 0 && Math.abs(yDiff / xDiff) > 1){
+        direction = 'up';
+    } else if (xDiff > 0 && Math.abs(yDiff / xDiff) < 1 ){
+        direction = 'right';
+    } else if (yDiff > 0 && Math.abs(yDiff / xDiff) > 1){
+        direction = 'down';
+    } else {
+        direction = 'left';
+    }
+    this.console.log(xDiff, yDiff, direction);
+    
+    if(xDiff === 0 && yDiff === 0){
+        return; // 제자리 클릭일 경우
+    }
+
+    var newData = [
+        [],
+        [],
+        [],
+        []
+      ];
+    switch (direction) {
+        case 'left':
+          data.forEach(function(colData, i) {
+            colData.forEach(function(rowData, j) {
+              if (rowData) {
+                if (newData[i][newData[i].length - 1] && newData[i][newData[i].length - 1] === rowData) {
+                    newData[i][newData[i].length - 1] *= 2;
+                  var currentScore = parseInt(score.textContent, 10);
+                  score.textContent = currentScore + newData[i][newData[i].length - 1];
+                } else {
+                    newData[i].push(rowData);
+                }
+              }
+            });
+          });
+          console.log(newData);
+          [1, 2, 3, 4].forEach(function(colData, i) {
+            [1, 2, 3, 4].forEach(function(rowData, j) {
+              data[i][j] = newData[i][j] || 0;
+            });
+          });
+          break;
+        case 'right':
+          data.forEach(function(colData, i) {
+            colData.forEach(function(rowData, j) {
+              if (rowData) {
+                if (newData[i][0] && newData[i][0] === rowData) {
+                    newData[i][0] *= 2;
+                  var currentScore = parseInt(score.textContent, 10);
+                  score.textContent = currentScore + newData[i][0];
+                } else {
+                    newData[i].unshift(rowData);
+                }
+              }
+            });
+          });
+          console.log(newData);
+          [1, 2, 3, 4].forEach(function(colData, i) {
+            [1, 2, 3, 4].forEach(function(rowData, j) {
+              data[i][3 - j] = newData[i][j] || 0;
+            });
+          });
+          break;
+        case 'up':
+          data.forEach(function(colData, i) {
+            colData.forEach(function(rowData, j) {
+              if (rowData) {
+                if (newData[j][newData[j].length - 1] && newData[j][newData[j].length - 1] === rowData) {
+                    newData[j][newData[j].length - 1] *= 2;
+                  var currentScore = parseInt(score.textContent, 10);
+                  score.textContent = currentScore + newData[j][newData[j].length - 1];
+                } else {
+                  newData[j].push(rowData);
+                }
+              }
+            });
+          });
+          console.log(newData);
+          [1, 2, 3, 4].forEach(function(rowData, i) {
+            [1, 2, 3, 4].forEach(function(colData, j) {
+              data[j][i] = newData[i][j] || 0;
+            });
+          });
+          break;
+        case 'down':
+          data.forEach(function(colData, i) {
+            colData.forEach(function(rowData, j) {
+              if (rowData) {
+                if (newData[j][0] && newData[j][0] === rowData) {
+                  newData[j][0] *= 2;
+                  var currentScore = parseInt(score.textContent, 10);
+                  score.textContent = currentScore + newData[j][0];
+                } else {
+                  newData[j].unshift(rowData);
+                }
+              }
+            });
+          });
+          console.log(newData);
+          [1, 2, 3, 4].forEach(function(rowData, i) {
+            [1, 2, 3, 4].forEach(function(colData, j) {
+              data[3 - j][i] = newData[i][j] || 0;
+            });
+          });
+          break;
+      }
+    makeRandomCell();
+});
+
+
 initSetting();
-makeRandom();
+makeRandomCell();
 drawing();
